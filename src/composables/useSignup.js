968ed3fd0b463702b15e33ce2e80929c2b9069d4
SoerenBak <fff56k@gmail.com>
2024-10-16 14:55:13 +0200

    Made firebase login and signup forms

---
+++ src/composables/useSignup.js
@@ -0,0 +1,30 @@
+import { ref } from 'vue';
+import { projectAuth } from '../firebase/config';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+
+const error = ref(null);
+
+const signup = async (email, password, displayName) => {
+  error.value = null;
+
+  try {
+    const res = await createUserWithEmailAndPassword(projectAuth, email, password);
+    if (!res) {
+      throw new Error('Could not complete signup');
+    }
+
+    await updateProfile(res.user, { displayName });
+    error.value = null;
+
+    return res;
+  } catch (err) {
+    console.log(err.message);
+    error.value = err.message;
+  }
+};
+
+const useSignup = () => {
+  return { error, signup };
+};
+
+export default useSignup;
---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+// Import the necessary functions from Firebase SDK
+import { initializeApp } from 'firebase/app';
+import { getFirestore, serverTimestamp } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD7lIDKZ6Y0c2LuvErDxV4Wb3tEpwHraWg",
+    authDomain: "medical-dashboard-fd7a3.firebaseapp.com",
+    projectId: "medical-dashboard-fd7a3",
+    storageBucket: "medical-dashboard-fd7a3.appspot.com",
+    messagingSenderId: "228102247025",
+    appId: "1:228102247025:web:0b5553014da171778501d0"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Firestore and Auth services
+const projectAuth = getAuth(firebaseApp);
+const projectFirestore = getFirestore(firebaseApp);
+
+// Get server timestamp
+const timestamp = serverTimestamp();
+
+// Exporting the initialized services
+export { projectAuth, projectFirestore, timestamp };
